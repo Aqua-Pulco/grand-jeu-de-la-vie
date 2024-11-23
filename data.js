c76a02ddff66b7d13745ab9c0c9a177577c18b97
AquaPulco <iris.yolal@gmail.com>
2024-11-23 15:21:56 +0100

    camille modif

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ export const riche = {
     "tu es Encarté·e au MEDEF": {
         tirage_max: 3,
         action: [
-            "Trouve les autres patrons du medef, les politiques ripoux et les mafieux présents ce soir :\nensemble faites une OPA sur un bien essentiel de la soirée\n(alcool,  son ou encore petits fours... à votre guise),\ntente de prendre la plus grosse part du gateau."
+            "Trouve les autres patrons du MEDEF, les politiques ripoux et les mafieux présents ce soir :\nensemble faites une OPA sur un bien essentiel de la soirée\n(alcool, son ou encore petits fours... à votre guise),\ntente de prendre la plus grosse part du gâteau."
         ],
         // image: "" // lien vers l'image
     },
@@ -17,21 +17,21 @@ export const riche = {
     "En Grande-Bretagne, on l’appelle « la gauche champagne »,\nen Allemagne « la fraction Toscane »,\naux États-Unis « les libéraux Limousine » ou « le gauchisme de Park Avenue ».\nEn France, vous êtes la « gauche caviar ».": {
         tirage_max: 3,
         action: [
-            "Affronte un·e catho versaillais·e en shifu-Re-mix :\nwokiste > féminazie > islamo-gauchiste > wokiste.\nSi tu perds, tu devras passer 'Olympics' d'Arielle .D."
+            "Affronte un·e catho versaillais·e en shifu-Re-mix :\nwokiste > féminazie > islamo-gauchiste > wokiste.\nSi tu perds, tu devras passer 'Olympics' d'Arielle D."
         ],
         // image: "" // lien vers l'image
     },
     "tes revenus proviennent d'activités illégales": {
         tirage_max: 3,
         action: [
-            "Trouve les autres mafieux, les patrons du medef et les politiques ripoux :\nensemble faites une OPA sur un bien essentiel de la soirée\n(alcool,  son ou encore petits fours... à votre guise),\ntente de prendre la plus grosse part du gateau."
+            "Trouve les autres mafieux, les patrons du MEDEF et les politiques ripoux :\nensemble faites une OPA sur un bien essentiel de la soirée\n(alcool, son ou encore petits fours... à votre guise),\ntente de prendre la plus grosse part du gâteau."
         ],
         // image: "" // lien vers l'image
     },
     "tu es politique ripoux": {
         tirage_max: 3,
         action: [
-            "Au cours de la soirée et à la même personne,\ndis une chose, puis (plus tard ou tout de suite), son contraire.\nRends également un petit service aux mafieux et aux encartés au MEDEF dans leur mission,\ntente de prendre la plus grosse part du gateau."
+            "Au cours de la soirée et à la même personne,\ndis une chose, puis (plus tard ou tout de suite), son contraire.\nRends également un petit service aux mafieux et aux encarté·es au MEDEF dans leur mission,\ntente de prendre la plus grosse part du gâteau."
         ],
         // image: "" // lien vers l'image
     },
@@ -59,11 +59,11 @@ export const riche = {
     "tu es de droite": {
         tirage_max: 3,
         action: [
-            "Trouve un·e 'Marxiste-Leniniste' dans la soirée, glisse lui :\n'C'est normal d'être de gauche quand on est jeune... Apres, on grandit.'\nTrouve un.e artiste-auter.ice, demande lui si il/elle ne voudrait pas trouver un vrai travail, plutot."
+            "Trouve un·e 'Marxiste-Leniniste' dans la soirée, glisse lui :\n'C'est normal d'être de gauche quand on est jeune... Après, on grandit.'\nTrouve un·e 'artiste-auter·ice', demande-lui si il/elle ne voudrait pas trouver un vrai travail, plutôt."
         ],
         // image: "" // lien vers l'image
     },
-    "tu es gouroux de secte BIO-GA": {
+    "tu es gourou de secte BIO-GA": {
         tirage_max: 3,
         action: [
             "Trouve tes adeptes et obtiens d'eux un chèque à l'ordre de Gaïa la Terre inc."
@@ -73,14 +73,14 @@ export const riche = {
     "tu es rappeur·euse de Neuilly-sur-Seine": {
         tirage_max: 3,
         action: [
-            "Dis 'wesh' et 'frère' au moins 10 fois (de chaque) au cours d'une conversation."
+            "Dis 'wesh' et 'frère' au moins 10 fois (de chaque) au cours d'une conversation.\nsi on te demande, ton single vient de sortir : 'Le ghetto' "
         ],
         // image: "" // lien vers l'image
     },
-    "tu es Catho Versaillais.e": {
+    "tu es Catho Versaillais·e": {
         tirage_max: 3,
         action: [
-            "Affronte quelqu'un·e de la gauche caviar en shifu-re-mix :\nwokiste > féminazie > islamo-gauchiste > wokiste etc.\nSi tu perds, tu devras passer 'Vois sur ton chemin'.\nndrl Les Choristes."
+            "Affronte quelqu'un·e de la gauche caviar \nen shifu-re-mix :\nwokiste > féminazie > islamo-gauchiste > wokiste etc.\nSi tu perds, tu devras passer 'Vois sur ton chemin'.\nndrl Les Choristes."
         ],
         // image: "" // lien vers l'image
     }
@@ -89,9 +89,9 @@ export const riche = {
 // Définition des catégories pauvres
 export const pauvre = {
     "tu es de toutes les manifs": {
-        tirage_max: 3,
+        tirage_max: 4,
         action: [
-            "Trouves et allie-toi avec le syndicaliste pour récupérer ce que les patrons du medef,\nles politiques ripoux et les mafieux ont volé dans la soirée."
+            "Trouve et allie-toi avec la·e syndicaliste pour récupérer ce que les patrons du MEDEF,\nles politiques ripoux et les mafieux ont volé dans la soirée."
         ],
         // image: "" // lien vers l'image
     },
@@ -103,28 +103,28 @@ export const pauvre = {
         // image: "" // lien vers l'image
     },
     "tu es intermittent·e de Montreuil": {
-        tirage_max: 3,
+        tirage_max: 4,
         action: [
             "Hacke la soirée :\nCe soir en fait, c'est ton pot de départ car tu pars t'installer à Marseille."
         ],
         // image: "" // lien vers l'image
     },
     "tu es militant activiste": {
-        tirage_max: 3,
+        tirage_max: 4,
         action: [
-            "Crée un collectif de ton choix avec un maximum de gens,\nsi tu atteins au moins 10 adhérents passe 'Planète Brûlée'."
+            "Crée un collectif de ton choix avec un maximum de gens,\nsi tu atteins au moins 10 adhérent·es passe 'Planète Brûlée'."
         ],
         // image: "" // lien vers l'image
     },
-    "tu es décroissant.e": {
-        tirage_max: 3,
+    "tu es décroissant·e": {
+        tirage_max: 4,
         action: [
             "N'amène rien à la soirée, tu peux également réutiliser un déguisement d'une soirée déguisée passée, quel que soit son thème"
         ],
         // image: "" // lien vers l'image
     },
     "tu es altermondialiste": {
-        tirage_max: 3,
+        tirage_max: 4,
         action: [
             "arrange-toi pour faire signer une pétition\n(une de ton invention OU une vraie qui existe)\nsi tu as moins de 10 signatures, tu devras faire semblant\nde vouloir passer une chanson de Zaz, ou pire...\nLa passer."
         ],
@@ -140,11 +140,11 @@ export const pauvre = {
     "tu es docteur·e en thèse sur les cheveliers paysans de l'an mille au lac de Paladru": {
         tirage_max: 3,
         action: [
-            "Si quelqu'un te pose la quesion 'qu'est-ce que tu fais dans la vie ?', tente d'expliquer ton sujet de thèse."
+            "Si quelqu'un te pose la question 'qu'est-ce que tu fais dans la vie ?', tente d'expliquer ton sujet de thèse."
         ],
         // image: "" // lien vers l'image
     },
-    "tu es Bartender, DJ, Performeur·euse, prof de yoga, tatoueur, astrologue-essayiste, naturopathe, equicoach, ceramiste, tiktokeur·euse, sexologue et apiculteur.ice en milieu urbain.": {
+    "tu es Bartender, DJ, Performeur·euse, prof de yoga, tatoueur, astrologue-essayiste, naturopathe, equicoach, céramiste, tiktokeur·euse, sexologue et apiculteur.ice en milieu urbain.": {
         tirage_max: 3,
         action: [
             "Demande à chaque personne de la soirée 'et toi, qu'est-ce que tu fais dans la vie ?'"
@@ -152,21 +152,21 @@ export const pauvre = {
         // image: "" // lien vers l'image
     },
     "tu es syndicaliste": {
-        tirage_max: 3,
+        tirage_max: 4,
         action: [
-            "Allie-toi avec la personne de la soirée qui est « de toutes les manifs » pour récupérer ce que les patrons du Medef, les politiques ripoux et les mafieux ont volé dans la soirée."
+            "Allie-toi avec la personne de la soirée qui est « de toutes les manifs » pour récupérer ce que les patrons du MEDEF, les politiques ripoux et les mafieux ont volé dans la soirée."
         ],
         // image: "" // lien vers l'image
     },
     "tu es adepte d'une secte BYO-GA": {
-        tirage_max: 3,
+        tirage_max: 4,
         action: [
             "Ramène ton chéquier."
         ],
         // image: "" // lien vers l'image
     },
-    "tu es artiste-auteurice": {
-        tirage_max: 3,
+    "tu es artiste-auteur·ice": {
+        tirage_max: 4,
         action: [
             "Gagne 5 followers sur Insta."
         ],
